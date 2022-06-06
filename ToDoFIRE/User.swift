25a216c00a0a8fd3de8a01a8fd9803f1778767c1
Slava <Slava7711240@gmail.com>
2022-06-06 21:46:33 +0300

    Created task and user form

---
+++ ToDoFIRE/User.swift
@@ -0,0 +1,21 @@
+//
+//  User.swift
+//  ToDoFIRE
+//
+//  Created by Slava Orlov on 06.06.2022.
+//
+
+import Foundation
+import Firebase
+
+
+struct User {
+    
+    let uid: String
+    let email: String
+    
+    init(user: User) {
+        self.uid = user.uid
+        self.email = user.email!
+    }
+}
